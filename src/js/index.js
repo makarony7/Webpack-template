2fd5776dc77ca8e4c582527b5005fd87a99c08f4
Janek <makarony7@wp.pl>
2020-01-26 17:39:52 +0100

    added some function

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,21 @@ var yellow = document.getElementById('yellow')
 yellow.innerHTML= 'A yellow webpack template';
 
 [1, 2, 3].map((n) => n + 1);
+
+// show date function
+const clickButton = document.getElementById('clickButton')
+const showDate = document.getElementById('showDate')
+const closeDate = document.getElementById('closeDate')
+
+clickButton.addEventListener("click", showDateHour);
+function showDateHour() {
+    showDate.innerHTML = Date();
+    closeDate.classList.add("d-block");
+}
+
+// hide date function
+closeDate.addEventListener("click", hideDateHour);
+function hideDateHour() {
+    showDate.innerHTML = '';
+    closeDate.classList.remove("d-block");
+}
